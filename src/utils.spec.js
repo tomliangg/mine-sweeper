b337d1d62d71c5692d9190df5485ca612ce5e287
Tom Liang <tomliangg@gmail.com>
2021-09-18 22:03:41 -0700

    add test for generateMinesByNumber utils function

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,7 +1,7 @@
-import { generateMinesweeper } from "./util";
+import { generateMinesweeper, generateMinesByNumber } from "./util";
 
-describe("utils functions", () => {
-  test("generateMinesweeper should generate the board with mines correctly", () => {
+describe("generateMinesweeper function", () => {
+  test("should generate the board with mines correctly", () => {
     expect(generateMinesweeper(5, [[0, 0], [2, 2], [2, 3]]))
       .toEqual([
         ["m", 1, 0, 0, 0],
@@ -10,13 +10,42 @@ describe("utils functions", () => {
         [0, 1, 2, 2, 1],
         [0, 0, 0, 0, 0]
       ]);
+
+    expect(generateMinesweeper(3, [[1, 1]]))
+    .toEqual([
+      [1, 1, 1],
+      [1, "m", 1],
+      [1, 1, 1],
+    ]);
   });
 
-  test("generateMinesweeper should generate the board without mines correctly", () => {
+  test("should generate the board without mines correctly", () => {
     expect(generateMinesweeper(2, []))
       .toEqual([
         [0, 0],
         [0, 0]
       ]);
   });
-})
+});
+
+describe("generateMinesByNumber function", () => {
+  test("should generate correct number of mines", () => {
+    const SIZE = 5;
+    const NUM_1 = 1;
+    const NUM_2 = 2;
+
+    expect(generateMinesByNumber(SIZE, NUM_1)).toHaveLength(NUM_1);
+    expect(generateMinesByNumber(SIZE, NUM_2)).toHaveLength(NUM_2);
+  });
+
+  test("generated mines should be within size range", () => {
+    const SIZE = 5;
+    const NUM = 5;
+
+    const mines = generateMinesByNumber(SIZE, NUM);
+    mines.forEach(mine => {
+      expect(mine[0]).toBeLessThan(SIZE);
+      expect(mine[1]).toBeLessThan(SIZE);
+    });
+  });
+});
